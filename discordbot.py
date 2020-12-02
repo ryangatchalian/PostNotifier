176350524fb83faa240cbf42edab023bc3c8f63e
ryangatchalian <73212852+ryangatchalian@users.noreply.github.com>
2020-12-02 05:12:55 -0500

    bot fixes

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ import settings
 import post
 import discord
 import reddit
+from discord.ext import commands
 import os
 
 
@@ -40,59 +41,20 @@ def scrape_sites(subname=search):
     db.close()
     return first_count, latest_count
 
-
-TOKEN = 'Nzc1NjUwMDU4NTM5NjMwNjMy.X6paaA.pbALCiGjFi2TZN0Qn54uyJeCd9U'
-
-client = discord.Client()
+client = commands.Bot(command_prefix=".")
+token = "DISCORD_BOT_TOKEN"
 
 @client.event
 async def on_ready():
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("Bot is ready.")
 
-@client.event
-async def on_message(message):
-    global channel_name, channel_id, is_running, KEYWORD, search
-    if message.author == client.user:
-        return
-
-    if 'run here' == message.content:
-        channel_name = message.channel
-        channel_id = message.channel.id
-        print(channel_name)
-        await message.channel.send("Posts will be added to this channel. Thanks!")
-
-    if message.content == 'stop scrape':
-        is_running = False
-        await message.channel.send("Scraping stopped.")
-
-    if message.content == 'start scrape':
-        is_running = True
-        await message.channel.send("Starting scrape!")
-        await start_scrape(message.channel)
-
-    if "key" in message.content:
-        msg = message.content
-        if is_running:
-            await message.channel.send("Please stop the scrape first!")
-        else:
-            KEYWORD = msg.replace('key ', '')
-            print(KEYWORD)
-            await message.channel.send("Keyword updated. Run the script when ready!")
-
-    if "-subreddit" in message.content:
-        msg = message.content
-        if is_running:
-            await message.channel.send("Please stop the scrape first!")
-        else:
-            search = msg.replace('-subreddit ', '')
-            await message.channel.send("Subreddit updated. Run the script when ready!")
-
-async def start_scrape(channel):
+@client.command()
+async def scrape(ctx):
     global is_running, search
+    is_running = True
     while is_running:
         ids = scrape_sites(search)
-        # db = post.create_db_connection('localhost', 'root', settings.MYSQL_PASS, settings.DB_NAME)
-        # USING HEROKU
         db = post.create_db_connection('us-cdbr-east-02.cleardb.com', 'b8b2fcd9b2a95d', 'dfdbcc4a', 'heroku_3f0223b7f1eb928')
         start, stop = ids
         print(start, stop)
@@ -102,7 +64,32 @@ async def start_scrape(channel):
             output = post.latest_posts(db, search, start, stop)
             print(output)
             for row in output:
-                await channel.send(f'{row[1]} \n {row[2]} \n {row[3]}\n')
+                await ctx.send(f'{row[1]} \n {row[2]} \n {row[3]}\n')
         db.close()
 
-client.run(TOKEN)
+@client.command()
+async def stop(ctx):
+    global is_running
+    is_running = False
+    await ctx.send("Scraping stopped.")
+
+@client.command()
+async def key(ctx, newkey):
+    global is_running, KEYWORD
+    if is_running:
+        await ctx.send("Please stop the scrape first!")
+    else:
+        KEYWORD = newkey
+        await ctx.send("Keyword updated. Run the script when ready!")
+
+@client.command()
+async def key(ctx, newkey):
+    global is_running, search
+    if is_running:
+        await ctx.send("Please stop the scrape first!")
+    else:
+        search = newkey
+        await ctx.send("Keyword updated. Run the script when ready!")
+
+
+client.run(token)
